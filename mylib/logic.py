81806176c67253751adaf948113b9bf3a48b5e4b
vsankumar <161472662+vsankumar@users.noreply.github.com>
2024-03-03 17:39:17 +0000

    test module added

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,4 +1,5 @@
 import wikipedia
+from textblob import TextBlob
 
 
 def wiki(name="War Goddess", length=1):
@@ -6,8 +7,16 @@ def wiki(name="War Goddess", length=1):
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
 
+
 def search_wiki(name):
     """This is wikipedia search"""
-    
+
     result = wikipedia.search(name)
     return result
+
+
+def phrase(name):
+    """Returns the phrase"""
+    page = wiki(name)
+    blob = TextBlob(page)
+    return blob.noun_phrases
---
+++ test_main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from main import app
+
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Wikipedia API.  Call /search or /wiki"}
+
+
+def test_read_phrase():
+    response = client.get("/phrase/Sachin Tendulkar")
+    assert response.status_code == 200
+    assert response.json() == {
+        "result": [
+            "sachin ramesh tendulkar",
+            "[ sətɕin teːɳɖulkəɾ ]",
+            "april",
+            "international cricketer",
+            "indian national team",
+        ]
+    }
