d4afed7d5bb088c23a901636c697d3aee7d5b08d
vsankumar <161472662+vsankumar@users.noreply.github.com>
2024-03-03 14:55:51 +0000

    enhanced cli functionalty

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,9 @@ def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """This is wikipedia search"""
+    
+    result = wikipedia.search(name)
+    return result
